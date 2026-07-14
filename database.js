5eba08aa12bd148d5fc19760f9f86373dc73f88a
mahesh <maheshpalakunda.com>
2026-07-14 13:41:02 +0530

    Added GitHub Actions

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/shopnest', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://localhost:27017/shopnest', {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true
+// });
 
 // Customer Schema
 const customerSchema = new mongoose.Schema({
